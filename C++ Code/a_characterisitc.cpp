c4c95cdaecd9b85be5d67582f18df8b14b7f2cd5
samyak jain <2samyakjain@gmail.com>
2023-05-16 23:02:13 +0530

    s

--- C++ Code/a_characterisitc.cpp
+++ C++ Code/a_characterisitc.cpp
@@ -1,50 +1,41 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int fact(int n) {
-   if (n == 0 || n == 1)
-   return 1;
-   else
-   return n * fact(n - 1);
-}
+#define int long long
 
-int comb(int x, int y){
-   int ans = fact(x) / (fact(y) * fact(x-y));
-   return ans;
-}
-int main() {
-
-   int n;
-   cin>>n;
-   int k;
-   cin>>k;
-   int hola;
-   int flag=0;
-   for (int i = 0; i < n; ++i)
-   {
-      hola=0;
-      for (int j = 0; j <n; ++j)
+signed main(){
+   int t;
+   cin>>t;
+   while(t--){
+      int n;
+      int k;
+      cin>>n>>k;
+      int flag=0;
+      int x;
+      for (int i = 1; i < n+2; ++i)
       {
-         hola = comb(j,2)+comb(n-j,2);
-         if(hola==k){
+         int ans = (i*(i-1)/2)+((n-i)*(n-i-1)/2);
+         if(k==ans){
+            x=i;
+            flag=1;
             break;
-            flag==1;
-         }
-         else{
-            hola=0;
          }
       }
 
+      if (flag==1){
+         cout<<"yes"<<endl;
+         for (int i = 0; i < x; ++i)
+         {
+            cout<<1<<" ";
+         }
+         for (int i = x; i < n; ++i)
+         {
+            cout<<-1<<" ";
+         }
+         cout<<endl;
+      }
+      else{
+         cout<<"no"<<endl;
+      }
    }
-   int x,y;
-   if (flag==1){
-      cout<<"yes"<<endl;
-   }
-   else{
-      cout<<"no"<<endl;
-   }
-   
-   // int hola = comb(n,r);
-   cout<<hola;
-   
-   return 0;
 }
