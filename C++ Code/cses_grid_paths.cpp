57194c238546398ad51e0d59a464099c3ab6cf38
Samyak jain <2samyakjain@gmail.com>
2023-08-06 14:40:22 +0530

    code

--- C++ Code/cses_grid_paths.cpp
+++ C++ Code/cses_grid_paths.cpp
@@ -19,23 +19,23 @@ using namespace std;
 #define ss         second
 #define set_bits __builtin_popcountll
 #define all(x) (x).begin(), (x).end()
- 
-/*...............................................................*/
-ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
-ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
-void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
-ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
-ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
-ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
-vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
-ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-void google(int t) {cout << "Case #" << t << ": ";}
 
-bool prime(int n){
+/*...............................................................*/
+ll gcd(ll a, ll b) { if (b > a) { return gcd(b, a); } if (b == 0) { return a; } return gcd(b, a % b); }
+ll expo(ll a, ll b, ll mod) { ll res = 1; while (b > 0) { if (b & 1) res = (res * a) % mod; a = (a * a) % mod; b = b >> 1; } return res; }
+void extendgcd(ll a, ll b, ll* v) { if (b == 0) { v[0] = 1; v[1] = 0; v[2] = a; return; } extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return; } //pass an arry of size1 3
+ll mminv(ll a, ll b) { ll arr[3]; extendgcd(a, b, arr); return arr[0]; } //for non prime b
+ll mminvprime(ll a, ll b) { return expo(a, b - 2, b); }
+ll combination(ll n, ll r, ll m, ll* fact, ll* ifact) { ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m; }
+vector<ll> sieve(int n) { int* arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++) if (arr[i] == 0) { vect.push_back(i); for (int j = 2 * i; j <= n; j += i) arr[j] = 1; } return vect; }
+ll mod_add(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+ll mod_sub(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+ll mod_div(ll a, ll b, ll m) { a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m; }  //only for prime m
+ll phin(ll n) { ll number = n; if (n % 2 == 0) { number /= 2; while (n % 2 == 0) n /= 2; } for (ll i = 3; i <= sqrt(n); i += 2) { if (n % i == 0) { while (n % i == 0) n /= i; number = (number / i * (i - 1)); } } if (n > 1) number = (number / n * (n - 1)); return number; } //O(sqrt(N))
+void google(int t) { cout << "Case #" << t << ": "; }
+
+bool prime(int n) {
     if (n <= 1)
         return false;
     if (n <= 3)
@@ -48,62 +48,50 @@ bool prime(int n){
     return true;
 }
 /*.....................................................*/
-int f(vector<vector<char>>&grid, int i, int j){
+
+int f(vector<vector<char>>& grid, vector<vector<int>>& dp, int i, int j) {
     int n = grid.size();
-    if(i<0 or j<0 or j>=n or i>=n){
+    if (i < 0 || j < 0 || j >= n || i >= n) {
         return 0;
     }
+    if (grid[i][j] == '1') {
+        return dp[i][j] = 0;
+    }
+    if (i == 0 && j == 0) {
+        return 1;
+    }
     
+    if (dp[i][j] != -1) {
+        return dp[i][j]%MOD;
+    }
+    return dp[i][j] = (f(grid, dp, i - 1, j) + f(grid, dp, i, j - 1))%MOD;
 }
-void solve(int t){
+
+void solve(int t) {
     int n;
-    cin>>n;
-    vector<vector<char>>grid(n, vector<char>(n, '0'));
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            cin>>grid[i][j];
-            if(grid[i][j] == '.'){
+    cin >> n;
+    vector<vector<char>> grid(n, vector<char>(n, '0'));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> grid[i][j];
+            if (grid[i][j] == '.') {
                 grid[i][j] = '0';
             }
-            else{
+            else {
                 grid[i][j] = '1';
             }
         }
     }
 
-    vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-
-    // for (int i = 0; i < n; ++i)
-    // {
-    //     for (int j = 0; j < n; ++j)
-    //     {
-    //         cout<<grid[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+    cout << f(grid, dp, n - 1, n - 1)%MOD;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-signed main(){
+signed main() {
     fastio();
-    int t=1;
+    int t = 1;
     // cin>>t;
-    for(int i=0;i<t;i++){
+    for (int i = 0; i < t; i++) {
         solve(t);
     }
 }
-
