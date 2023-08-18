6a533a3f6897545429bb53c5ada360887fd846bd
Samyak jain <2samyakjain@gmail.com>
2023-08-18 23:43:33 +0530

    code

--- C++ Code/template.cpp
+++ C++ Code/template.cpp
@@ -1,75 +1,85 @@
-#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+
+#include<bits/stdc++.h>
+
 using namespace std;
- 
+using namespace chrono;
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define int        long long
-#define ll         long long
-#define lld        long double
-#define PI         3.141592653589793238462
-#define pb         push_back
-#define sz(x)     ((int)(x).size())
-#define INF     1e18
-#define MOD     1000000007
-#define MOD1     998244353
-#define INF     1e18
-#define nline     "\n"
-#define pb         push_back
-#define ppb     pop_back
-#define ff         first
-#define ss         second
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl '\n'
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
 #define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
- 
-/*...............................................................*/
-ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
-ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
-void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
-ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
-ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
-ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
-vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
-ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-void google(int t) {cout << "Case #" << t << ": ";}
+#define clr(x) memset(x, 0, sizeof(x))
 
-bool prime(int n){
-    if (n <= 1)
-        return false;
-    if (n <= 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-    return true;
-}
-/*.....................................................*/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
 
-void solve(int t){}
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-
-
-
-
-
-
-
-
-
-
-signed main(){
-    fastio();
-    int t=1;
-    cin>>t;
-    for(int i=0;i<t;i++){
-        solve(t);
-    }
+void solve()
+{
+    vector<int> a(10000, -3);
+    map<int, int> mp;
+    mp[3] = 5;
+    mp[2] = 5;
+    mp[1] = 5;
+    debug(mp)
+    cout<<4<<endl;
 }
 
+int main() 
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);   
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+    #endif
+        fastio();
+        auto start1 = high_resolution_clock::now();
+        int tt = 1;
+        cin >> tt;
+        while (tt--)
+        {
+            solve();
+        }
+        auto stop1 = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop1 - start1);
+    #ifndef ONLINE_JUDGE
+        cerr << "Time: " << duration . count() / 1000 << endl;
+    #endif
+}
