bdd45ec69837913a8bd7d8fce4d7a35f2c90713c
Samyak jain <2samyakjain@gmail.com>
2023-10-01 11:45:41 +0530

    code

--- C++ Code/cses_string_matching.cpp
+++ C++ Code/cses_string_matching.cpp
@@ -1,97 +1,56 @@
-#pragma GCC optimize("O3,unroll-loops")
-
+#include <iostream>
+#include <vector>
+#include <set>
 #include<bits/stdc++.h>
 
 using namespace std;
-using namespace chrono;
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nl '\n'
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-#define clr(x) memset(x, 0, sizeof(x))
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
-#else
-#define debug(x);
-#endif
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
+    int t;
+    cin >> t;
 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
+    for (int c = 0; c < t; ++c) {
+        int n, m;
+        cin >> n >> m;
 
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+        set<int> myset;
+        int count = 0;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+        while (true) {
+            if (n % m == 0) {
+                cout << count << endl;
+                break;
+            }
 
-void solve()
-{
- string s;
- cin>>s;
- string m;
- cin>>m;
- int count=0;
- for (int i = 0; i < s.size(); i++)
- {
- 	// debug(m);
- 	// debug(s);
- 	string t = s.substr(i, m.size());
- 	debug(t);
- 	if(t==m){
- 		count++;
- 	}
- 	else{
- 		i=i+m.size();
- 	}
- }
- cout<<count<<endl;
-}
-
-int main() 
-{
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);   
-        freopen("output.txt", "w", stdout);
-        freopen("error.txt", "w", stderr);
-    #endif
-        fastio();
-        auto start1 = high_resolution_clock::now();
-        int tt = 1;
-        // cin >> tt;
-        while (tt--)
-        {
-            solve();
+            if (n > m) {
+                int l = myset.size();
+                myset.insert(n % m);
+                if (l == myset.size()) {
+                    cout << -1 << endl;
+                    break;
+                }
+                n = n % m;
+            } else {
+                int temp = n;
+                int power2greaterorequalthan = m / n;
+                if (power2greaterorequalthan == m / n) {
+                    power2greaterorequalthan -= 1;
+                }
+                int power2req = 0;
+                while (power2greaterorequalthan > 0) {
+                    power2greaterorequalthan /= 2;
+                    power2req += 1;
+                }
+                n = n * (1 << power2req);
+                count += temp * ((1 << power2req) - 1);
+                cout<<count<<endl;
+            }
         }
-        auto stop1 = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop1 - start1);
-    #ifndef ONLINE_JUDGE
-        cerr << "Time: " << duration . count() / 1000 << endl;
-    #endif
+    }
+    
+
+    return 0;
 }
--- C++ Code/python.py
+++ C++ Code/python.py
@@ -1,21 +1,9 @@
 import sys, os, io
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
- 
-number_of_testcases = int(input())
+import math
 
-for current_testcase_index in range(number_of_testcases):
-    arr = list(int (i) for i in input().split())
-    n = arr[0]
-    m = arr[1]
-    ratio = n/m
-    if(ratio%0.25!=0):
-        print(-1)
-    
-    else:
-        count = 0 
-        if(n%m==0):
-            count = 0
-            print(0)
-                
-        else:
-            print("hi")
+t  = int(input())
+arr = list(int (i) for i in input().split())
+apples = arr[0]
+people = arr[1]
+
