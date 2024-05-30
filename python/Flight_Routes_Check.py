dc36ce12e227b70a6741dd37050dedc44a8db907
Samyak jain <2samyakjain@gmail.com>
2024-05-30 16:04:21 +0530

    code

--- python/Flight_Routes_Check.py
+++ python/Flight_Routes_Check.py
@@ -1,23 +1,47 @@
-n, m = map(int, (input().split()))
-adj = {}
-for i in range(m):
-    adj[i] = []
-for i in range(m):
-    x, y = map(int, input().split())
-    adj[x-1].append(y-1)
-    
+from typing import List, Set
 
-def dfs(node):
-    visited.add(node)
-    for i in adj[node]:
-        if i not in visited:
-            dfs(i)
+n, m = map(int, input().split())
 
-myset = ()
-for i in range(n):
-    visited = ()
-    
-    
-    
-    
-    
+forward_graph = [[] for _ in range(n)]
+reverse_graph = [[] for _ in range(n)]
+
+for _ in range(m):
+	a, b = map(int, input().split())
+	forward_graph[a - 1].append(b - 1)
+	reverse_graph[b - 1].append(a - 1)
+
+
+def dfs(graph: List[List[int]]) -> Set[int]:
+	"""
+	Performs a depth-first traversal of a graph starting from node 0. Removes
+	each node reached from a set of all nodes. Once complete, the set will only
+	contain unvisited nodes and is then returned.
+
+	:param graph: An adjacency list representing a graph.
+	:return: A set containing all unvisited nodes in the depth-first traversal.
+	"""
+	unvisited = set(range(1, n))
+	stack = [0]
+	while stack:
+		curr = stack.pop()
+
+		for adj in graph[curr]:
+			if adj in unvisited:
+				stack.append(adj)
+				unvisited.remove(adj)
+
+	return unvisited
+
+
+# These will be empty if there are no unvisited nodes
+unvisited_fwd = dfs(forward_graph)
+unvisited_rev = dfs(reverse_graph)
+
+if unvisited_fwd:
+	print("NO")
+	print(f"1 {unvisited_fwd.pop() + 1}")
+elif unvisited_rev:
+	print("NO")
+	print(f"{unvisited_rev.pop() + 1} 1")
+else:
+	print("YES")
--- python/Problem_2_Icy_Perimeter.py
+++ python/Problem_2_Icy_Perimeter.py
@@ -1,12 +1,44 @@
 n = int(input())
 grid = []
-for i in range(n):
-    temp = list(input())
-    for i in range(len(temp)):
-        if temp[i] == '.':
-            temp[i] = 0
+for _ in range(n):
+    row = list(input().strip())
+    for i in range(len(row)):
+        if row[i] == '.':
+            row[i] = 0
         else:
-            temp[i] = 1
-    
-    grid.append(temp)
-print(grid)
+            row[i] = 1
+    grid.append(row)
+
+directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+from collections import deque
+
+def bfs(x, y):
+    area, peri = 0, 0
+    q = deque([(x, y)])
+    visited = set([(x, y)])
+    while q:
+        curr_x, curr_y = q.popleft()
+        for dx, dy in directions:
+            new_x, new_y = curr_x + dx, curr_y + dy
+            if (new_x, new_y) not in visited:
+                visited.add((new_x, new_y))
+                if 0 <= new_x < n and 0 <= new_y < n:
+                    if grid[new_x][new_y] == 1:
+                        area += 1
+                        q.append((new_x, new_y))
+                    else:
+                        peri += 1
+                else:
+                    peri += 1
+    return area, peri
+
+arr = []
+visited = set()
+for i in range(n):
+    for j in range(n):
+        if (i, j) not in visited:
+            visited.add((i, j))
+            if grid[i][j] == 1:
+                arr.append(bfs(i, j))
+
+print(max(arr))
