0610560a4df20204ab0377183b90381c2b4393eb
Samyak jain <2samyakjain@gmail.com>
2024-07-06 16:16:40 +0530

    code

---
+++ python/Building_Roads.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+adj = {}
+for i in range(n):
+    adj[i] = []
+for i in range(m):
+    x, y = map(int, input().split())
+    x-=1
+    y-=1
+    adj[x].append(y)
+    adj[y].append(x)
+print(adj)
+
+count = 0
+new_roads = []
+
+visited = []
+for node in range(n):
