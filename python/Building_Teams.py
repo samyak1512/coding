674d16e5cc9e3aab94329782abed6998bcf6f398
Samyak jain <2samyakjain@gmail.com>
2024-07-07 22:04:52 +0530

    code

--- python/Building_Teams.py
+++ python/Building_Teams.py
@@ -1,43 +1,88 @@
+# from collections import deque
+
+# def is_bipartite(n, adj):
+#     odd = [0] * n
+
+#     def bfs(start):
+#         q = deque([start])
+#         odd[start] = 1  # Start with one set
+#         while q:
+#             node = q.popleft()
+#             for neighbor in adj[node]:
+#                 if odd[neighbor] == 0:  # If neighbor is unvisited
+#                     odd[neighbor] = -odd[node]  # Assign opposite set
+#                     q.append(neighbor)
+#                 elif odd[neighbor] == odd[node]:  # If neighbor has the same set
+#                     return False
+#         return True
+
+#     for i in range(n):
+#         if odd[i] == 0:  # If node i is unvisited
+#             if not bfs(i):
+#                 return "IMPOSSIBLE"
+
+#     return odd
+
+
+# n, m = map(int, input().split())
+# adj = {i: [] for i in range(n)}
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     adj[a-1].append(b-1)
+#     adj[b-1].append(a-1)
+
+# result = is_bipartite(n, adj)
+# if result == "IMPOSSIBLE":
+#     print(result)
+# else:
+#     for i in range(len(result)):
+#         if result[i] == -1:
+#             result[i] = 2
+#     print(*result)
+
+# previous solution
+
+n,m = map(int, input().split())
+adj = {}
+for i in range(n):
+    adj[i] = []
+for i in range(m):
+    x, y = map(int, input().split())
+    x-=1
+    y-=1
+    adj[x].append(y)
+    adj[y].append(x)
 from collections import deque
-
-def is_bipartite(n, adj):
-    odd = [0] * n
-
+def isBartpite():
+    colour = [0]*n
     def bfs(start):
-        q = deque([start])
-        odd[start] = 1  # Start with one set
+        q = deque()
+        q.append(start)
+        colour[start] = 1
         while q:
             node = q.popleft()
-            for neighbor in adj[node]:
-                if odd[neighbor] == 0:  # If neighbor is unvisited
-                    odd[neighbor] = -odd[node]  # Assign opposite set
-                    q.append(neighbor)
-                elif odd[neighbor] == odd[node]:  # If neighbor has the same set
+            for niegh in adj[node]:
+                if colour[niegh] == 0:#unvisited node
+                    colour[niegh] = -colour[node]
+                    q.append(niegh)
+                elif colour[niegh] == colour[node]:
                     return False
         return True
-
     for i in range(n):
-        if odd[i] == 0:  # If node i is unvisited
+        if colour[i] == 0:
             if not bfs(i):
                 return "IMPOSSIBLE"
+    return colour
 
-    return odd
-
-
-n, m = map(int, input().split())
-adj = {i: [] for i in range(n)}
-
-for _ in range(m):
-    a, b = map(int, input().split())
-    adj[a-1].append(b-1)
-    adj[b-1].append(a-1)
-
-result = is_bipartite(n, adj)
-if result == "IMPOSSIBLE":
-    print(result)
+x = isBartpite()
+if x != "IMPOSSIBLE":
+    for i in range(len(x)):
+        if x[i] == -1:
+            x[i] = 2
+    print(*x)          
 else:
-    for i in range(len(result)):
-        if result[i] == -1:
-            result[i] = 2
-    print(*result)
-            
+    print(x)  
+                    
+        
+    
--- python/Flight_Routes_Check.py
+++ python/Flight_Routes_Check.py
@@ -1,47 +1,87 @@
-from typing import List, Set
+# from typing import List, Set
 
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
-forward_graph = [[] for _ in range(n)]
-reverse_graph = [[] for _ in range(n)]
+# forward_graph = [[] for _ in range(n)]
+# reverse_graph = [[] for _ in range(n)]
 
-for _ in range(m):
-	a, b = map(int, input().split())
-	forward_graph[a - 1].append(b - 1)
-	reverse_graph[b - 1].append(a - 1)
-
-
-def dfs(graph: List[List[int]]) -> Set[int]:
-	"""
-	Performs a depth-first traversal of a graph starting from node 0. Removes
-	each node reached from a set of all nodes. Once complete, the set will only
-	contain unvisited nodes and is then returned.
-
-	:param graph: An adjacency list representing a graph.
-	:return: A set containing all unvisited nodes in the depth-first traversal.
-	"""
-	unvisited = set(range(1, n))
-	stack = [0]
-	while stack:
-		curr = stack.pop()
-
-		for adj in graph[curr]:
-			if adj in unvisited:
-				stack.append(adj)
-				unvisited.remove(adj)
-
-	return unvisited
+# for _ in range(m):
+# 	a, b = map(int, input().split())
+# 	forward_graph[a - 1].append(b - 1)
+# 	reverse_graph[b - 1].append(a - 1)
 
 
-# These will be empty if there are no unvisited nodes
-unvisited_fwd = dfs(forward_graph)
-unvisited_rev = dfs(reverse_graph)
+# def dfs(graph: List[List[int]]) -> Set[int]:
+# 	"""
+# 	Performs a depth-first traversal of a graph starting from node 0. Removes
+# 	each node reached from a set of all nodes. Once complete, the set will only
+# 	contain unvisited nodes and is then returned.
 
-if unvisited_fwd:
-	print("NO")
-	print(f"1 {unvisited_fwd.pop() + 1}")
-elif unvisited_rev:
-	print("NO")
-	print(f"{unvisited_rev.pop() + 1} 1")
+# 	:param graph: An adjacency list representing a graph.
+# 	:return: A set containing all unvisited nodes in the depth-first traversal.
+# 	"""
+# 	unvisited = set(range(1, n))
+# 	stack = [0]
+# 	while stack:
+# 		curr = stack.pop()
+
+# 		for adj in graph[curr]:
+# 			if adj in unvisited:
+# 				stack.append(adj)
+# 				unvisited.remove(adj)
+
+# 	return unvisited
+
+
+# # These will be empty if there are no unvisited nodes
+# unvisited_fwd = dfs(forward_graph)
+# unvisited_rev = dfs(reverse_graph)
+
+# if unvisited_fwd:
+# 	print("NO")
+# 	print(f"1 {unvisited_fwd.pop() + 1}")
+# elif unvisited_rev:
+# 	print("NO")
+# 	print(f"{unvisited_rev.pop() + 1} 1")
+# else:
+# 	print("YES")
+
+# previous solution
+
+n,m = map(int, input().split())
+adj = {}
+rev_adj = {}
+for i in range(n):
+    adj[i] = []
+    rev_adj[i] = []
+for i in range(m):
+    x, y = map(int, input().split())
+    x-=1
+    y-=1
+    adj[i].append(x)
+    rev_adj[i].append(x)
+from collections import deque
+
+def bfs(node):
+    q = deque()
+    visited = set()
+    q.append(node)
+    while q:
+        q.popleft()
+        for neigh in adj[node]:
+            visited.add(neigh)
+    return visited
+
+visited = bfs(0)
+
+if len(visited) == n:
+    
+    
+
 else:
-	print("YES")
+    print("NO")
+    node = 0
+    for i in range(n):
+        if i not in visited:
+            node = i
+    print(1, node+1)
